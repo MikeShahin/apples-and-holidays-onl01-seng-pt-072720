b840234257b91451889157a507e8ffbc9e3334d2
Mike Shahindoust <gobias101@gmail.com>
2020-08-07 00:51:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season}:"
     holiday.each do |holiday, supplies|
-      puts " #{holiday"
+      puts "  #{holiday.to_s.split('_').map.join(' ') }: #{supplies.join(", ")}"
 end
 
 def all_holidays_with_bbq(holiday_hash)
